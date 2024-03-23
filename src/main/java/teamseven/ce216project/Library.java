1de28a9d2e3227500604d62706c93ab2de23ba69
Ahmet Çiftci <115403518+ahmet-ciftci@users.noreply.github.com>
2024-03-23 15:52:13 +0300

    addBook() and deleteBook() finished

--- src/main/java/teamseven/ce216project/Library.java
+++ src/main/java/teamseven/ce216project/Library.java
@@ -7,4 +7,25 @@ public class Library {
     private ArrayList<Book> foundBooks;
     private ArrayList<String> uniqueTags;
     private String jsonPath;
+    public void addBook(String title, String subtitle, String isbn, String publisher, String date, String edition, String numberOfPages, String cover, String coverPath, String language, String rating, ArrayList<String> authors, ArrayList<String> translators, ArrayList<String> tags) {
+        books.add(new Book(title, subtitle, isbn, publisher, date, edition, numberOfPages, cover, coverPath, language, rating, authors, translators, tags));
+        addTag(tags);
+        updateJson();
+    }
+
+    public void deleteBook(Book bookToDelete) {
+        books.remove(bookToDelete);
+
+        // Check if deleted book's tags appear in any existing book
+        ArrayList<String> tagsToCheck = bookToDelete.getTags();
+        tagChecker:
+        for (String tag : tagsToCheck) {
+            for (Book book : books) {
+                if (book.getTags().contains(tag)) {continue tagChecker;} // Proceed to check other tags if a tag is found in any existing book
+            }
+            uniqueTags.remove(tag); //Remove a tag if it has not been found in any existing book.
+        }
+
+        updateJson();
+    }
 }
