250176285edf4d49563ff07d072dafd52198eca2
Efes-P <efesonmez03@gmail.com>
2024-04-13 14:10:47 +0300

    Completed prototype search

--- src/main/java/teamseven/ce216project/Library.java
+++ src/main/java/teamseven/ce216project/Library.java
@@ -3,7 +3,6 @@ package teamseven.ce216project;
 
 import java.io.IOException;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.lang.reflect.Type;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -35,155 +34,106 @@ public class Library {
     }
 
     //Main Search Method
-    public void search(String string){
-
+    public void search(String string) {
         if (string == null) {
-            foundBooks=books;
-            return;
-        }else {
-            foundBooks.clear();
-
-           for (Book b : books) {
-                isFound = false;
-                if (!foundBooks.isEmpty()) {
-                    for (Book j : foundBooks) {
-                        if (j == b) {
-                            isFound = true;
-                            break;
-                        }
-                    }
-                }
-                if (!isFound) {
-                    searchForAuthor(string, b);
-                    searchForDate(string, b);
-                    searchForISBN(string, b);
-                    searchForEdition(string, b);
-                    searchForLanguage(string, b);
-                    searchForNumberOfPages(string, b);
-                    searchForPublisher(string, b);
-                    searchForRating(string, b);
-                    searchForSubtitle(string, b);
-                    searchForTitle(string, b);
-                    searchForTranslator(string, b);
-
-
-                }
+            foundBooks = books;
+        } else {
+            foundBooks = new ArrayList<>();
+            for (Book b : books) {
+                searchForAuthor(string, b);
+                searchForDate(string, b);
+                searchForISBN(string, b);
+                searchForEdition(string, b);
+                searchForLanguage(string, b);
+                searchForNumberOfPages(string, b);
+                searchForPublisher(string, b);
+                searchForRating(string, b);
+                searchForSubtitle(string, b);
+                searchForTitle(string, b);
+                searchForTranslator(string, b);
             }
         }
     }
-
 
 
     // Complementary Search Methods
-    private void searchForTitle(String string, Book book){
-        if(book.getTitle()!=null) {
-            if (book.getTitle().contains(string) && !isFound) {
-                foundBooks.add(book);
-                isFound = true;
-            }
+    private void searchForTitle(String string, Book book) {
+        if (book.getTitle() != null) {
+            if (book.getTitle().contains(string)) foundBooks.add(book);
         }
     }
 
-    private void searchForISBN(String string, Book book){
-        if(book.getIsbn()!=null) {
-            if (book.getIsbn().contains(string) && !isFound) {
-                foundBooks.add(book);
-                isFound = true;
-
-            }
+    private void searchForISBN(String string, Book book) {
+        if (book.getIsbn() != null) {
+            if (book.getIsbn().contains(string)) foundBooks.add(book);
         }
     }
 
-    private void searchForSubtitle(String string, Book book){
-        if(book.getSubtitle()!=null) {
-            if (book.getSubtitle().contains(string) && !isFound) {
-                foundBooks.add(book);
-                isFound = true;
+    private void searchForSubtitle(String string, Book book) {
+        if (book.getSubtitle() != null) {
+            if (book.getSubtitle().contains(string)) foundBooks.add(book);
 
-            }
         }
     }
 
-    private void searchForPublisher(String string, Book book){
-        if(book.getPublisher()!=null) {
-            if (book.getPublisher().contains(string) && !isFound) {
-                foundBooks.add(book);
-                isFound = true;
+    private void searchForPublisher(String string, Book book) {
+        if (book.getPublisher() != null) {
+            if (book.getPublisher().contains(string)) foundBooks.add(book);
 
-            }
         }
     }
 
-    private void searchForLanguage(String string, Book book){
-        if(book.getLanguage()!=null) {
-            if (book.getLanguage().contains(string) && !isFound) {
-                foundBooks.add(book);
-                isFound = true;
+    private void searchForLanguage(String string, Book book) {
+        if (book.getLanguage() != null) {
+            if (book.getLanguage().contains(string)) foundBooks.add(book);
 
-            }
         }
     }
 
-    private void searchForDate(String string, Book book){
-        if(book.getDate()!=null) {
-            if (book.getDate().contains(string) && !isFound) {
-                foundBooks.add(book);
-                isFound = true;
+    private void searchForDate(String string, Book book) {
+        if (book.getDate() != null) {
+            if (book.getDate().contains(string)) foundBooks.add(book);
 
-            }
         }
     }
 
-    private void searchForRating(String string, Book book){
-        if(book.getRating()!=null) {
-            float rating = Float.parseFloat(string);
-            float bookRating = Float.parseFloat(book.getRating());
-            if (bookRating == rating && !isFound) {
-                foundBooks.add(book);
-                isFound = true;
-            }
+    private void searchForRating(String string, Book book) {
+        if (book.getRating() != null) {
+            if (book.getRating().contains(string)) foundBooks.add(book);
         }
     }
 
-    private void searchForEdition(String string, Book book){
-        if(book.getEdition()!=null) {
-            int edition = Integer.parseInt(string);
-            int bookEdition = Integer.parseInt(book.getEdition());
-            if (bookEdition == edition && !isFound) {
-                foundBooks.add(book);
-                isFound = true;
-            }
+    private void searchForEdition(String string, Book book) {
+        if (book.getEdition() != null) {
+            if (book.getEdition().contains(string)) foundBooks.add(book);
+
         }
     }
 
-    private void searchForNumberOfPages(String string, Book book){
-        if(book.getNumberOfPages()!=null) {
-            int numberOfPages = Integer.parseInt(string);
-            int bookPages = Integer.parseInt(book.getNumberOfPages());
-            if (bookPages == numberOfPages && !isFound) {
-                foundBooks.add(book);
-                isFound = true;
-            }
+    private void searchForNumberOfPages(String string, Book book) {
+        if (book.getNumberOfPages() != null) {
+            if (book.getNumberOfPages().contains(string)) foundBooks.add(book);
+
         }
     }
 
-    private void searchForAuthor(String string, Book book){
-        if(book.getAuthors()!=null){
-            for(int i = 0; i<book.getAuthors().size();i++) {
-                 if (book.getAuthors().get(i).contains(string) && !isFound) {
+    private void searchForAuthor(String string, Book book) {
+        if (book.getAuthors() != null) {
+            for (int i = 0; i < book.getAuthors().size(); i++) {
+                if (book.getAuthors().get(i).contains(string)) {
                     foundBooks.add(book);
-                    isFound = true;
+
+                }
             }
-          }
         }
     }
 
-    private void searchForTranslator(String string, Book book){
-        if(book.getTranslators()!=null) {
+    private void searchForTranslator(String string, Book book) {
+        if (book.getTranslators() != null) {
             for (int i = 0; i < book.getTranslators().size(); i++) {
-                if (book.getTranslators().get(i).contains(string) && !isFound) {
+                if (book.getTranslators().get(i).contains(string)) {
                     foundBooks.add(book);
-                    isFound = true;
+
                 }
             }
         }
@@ -192,36 +142,46 @@ public class Library {
     //Tag Search
     //and Management
 
-    private void addTag(ArrayList<String> tags){
-        isFound=false;
-        for (String s: tags){
-            for (String uniqueS : uniqueTags){
-                if(uniqueS.equals(s))isFound=true;
+    public void addTag(Book book) {
+        isFound = false;
+        for (String s : book.getTags()) {
+            for (String uniqueS : uniqueTags) {
+                if (uniqueS.equals(s)) isFound = true;
             }
-            if(!isFound) {
+            if (!isFound) {
                 uniqueTags.add(s);
             }
         }
-        isFound=false;
+        isFound = false;
     }
 
-    private void filterByTags(ArrayList<String> tags) {
-        boolean fullyFound = false;
+    public void filterByTags(ArrayList<String> tags) {
+        foundBooks.clear();
+        ArrayList<Boolean> bool = new ArrayList<>();
         for (Book book : books) {
-            for (String s : tags) {
-                isFound = false;
-                for (String tag : book.getTags()) {
-                    if (tag.equals(s)) isFound = true;
+            if (book.getTags() != null) {
+                for (String s : book.getTags()) {
+                    for (String tag : tags) {
+                        if (s.equals(tag)) isFound = true;
+                    }
+                    bool.add(isFound);
                 }
-                if (!isFound) break;
+
+                for (Boolean b : bool) {
+                    if (!b) {
+                        isFound = false;
+                        break;
+                    }
+                }
+                if (isFound) foundBooks.add(book);
             }
-            if (!isFound) break;
-            else foundBooks.add(book);
         }
     }
 
 
-    public void updateJson(){       // Saves the .json the default file path
+
+
+    public void updateJson() {       // Saves the .json the default file path
         exportJson(jsonPath);
     }       //Saves the .json to the default path
 
@@ -232,11 +192,10 @@ public class Library {
         Formatter formatter = null;
         try {
             formatter = new Formatter(path);
-            formatter.format("%s",jsonFormat);      // Overwrites the existing .json
+            formatter.format("%s", jsonFormat);      // Overwrites the existing .json
         } catch (FileNotFoundException e) {
             System.err.println("File not found in path while exporting");
-        }
-        finally {
+        } finally {
             if (formatter != null) {
                 formatter.close();
             }
@@ -266,7 +225,9 @@ public class Library {
         }
     }
 
-    public void addBook(String title, String subtitle, String isbn, String publisher, String date, String edition, String numberOfPages, String cover, String coverPath, String language, String rating, ArrayList<String> authors, ArrayList<String> translators, ArrayList<String> tags) {
+    public void addBook(String title, String subtitle, String isbn, String publisher, String date, String
+            edition, String numberOfPages, String cover, String coverPath, String language, String
+                                rating, ArrayList<String> authors, ArrayList<String> translators, ArrayList<String> tags) {
         books.add(new Book(title, subtitle, isbn, publisher, date, edition, numberOfPages, cover, coverPath, language, rating, authors, translators, tags));
         //addTag(tags);
         updateJson();
@@ -281,7 +242,9 @@ public class Library {
         tagChecker:
         for (String tag : tagsToCheck) {
             for (Book book : books) {
-                if (book.getTags().contains(tag)) {continue tagChecker;} // Proceed to check other tags if a tag is found in any existing book
+                if (book.getTags().contains(tag)) {
+                    continue tagChecker;
+                } // Proceed to check other tags if a tag is found in any existing book
             }
             uniqueTags.remove(tag); //Remove a tag if it has not been found in any existing book.
         }
@@ -290,4 +253,5 @@ public class Library {
 
 
     }
+
 }
