9e3cefd16ec5da7f73e930d2b900767ae7fc9c32
MahmutOzgurKizil <ozgurkizil@hotmail.com>
2024-05-07 23:52:52 +0300

    Added new class for .jar

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module teamseven.ce216project {
     requires javafx.fxml;
     requires com.google.gson;
     requires java.desktop;
+    requires jdk.compiler;
 
     opens teamseven.ce216project to javafx.fxml,com.google.gson;
     exports teamseven.ce216project;
---
+++ src/main/java/teamseven/ce216project/ProxyMain.java
@@ -0,0 +1,7 @@
+package teamseven.ce216project;
+
+public class ProxyMain {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
